2d977faafe49cfb422a5e2933ddd57536116d355
JohnRussell13 <aleksandar.g1999@gmail.com>
2026-01-26 18:48:48 +0100

    Updated install script

--- post_install.sh
+++ post_install.sh
@@ -21,16 +21,38 @@ echo "$HOSTNAME" >/etc/hostname
 echo "==> Enabling services"
 systemctl enable NetworkManager
 
+echo "==> Setting root password"
+passwd
+
 echo "==> Installing packages"
 ./packages.sh
 
 echo "==> Creating user: $USERNAME"
 if ! id "$USERNAME" &>/dev/null; then
-    useradd -m -G wheel "$USERNAME"
+    useradd -m -g users -G nordvpn,uucp,input,wheel "$USERNAME"
     passwd "$USERNAME"
 fi
 
 echo "==> Configuring sudo"
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo "==> Setting image defaults for $USERNAME"
+su - "$USERNAME" -c '
+for t in jpeg png gif webp tiff; do
+  xdg-mime default imv.desktop image/$t
+done
+'
+
+echo "==> Installing GRUB (UEFI)"
+read -rp "EFI partition (e.g. /dev/sda1 or /dev/nvme0n1p1): " EFI_PART
+if [[ ! -b "$EFI_PART" ]]; then
+  echo "ERROR: Not a block device: $EFI_PART"
+  exit 1
+fi
+mkdir -p /boot/efi
+mount "$EFI_PART" /boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+umount /boot/efi
 
 echo "==> Done. Reboot when ready."
